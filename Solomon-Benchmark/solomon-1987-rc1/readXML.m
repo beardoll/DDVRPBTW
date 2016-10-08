b1f0c851ffe18cfc42d9bf8cfef9e5e9659fce13
beardoll <562969421@qq.com>
2016-10-08 17:39:09 +0800

    完成了xml数据的读取，现在在研究怎么批量读取xml文件以及批量写mat文件

--- Solomon-Benchmark/solomon-1987-rc1/readXML.m
+++ Solomon-Benchmark/solomon-1987-rc1/readXML.m
@@ -1,6 +1,6 @@
 % 读取xml文件
 
-infilename = '';   % 要读取文件名
+infilename = 'RC101_025.xml';   % 要读取文件名
 
 try
     xDoc = xmlread(infilename);
@@ -16,54 +16,80 @@ service_time = [];  % 存放服务时间
 
 allXcoordItems = xDoc.getElementsByTagName('cx');  % x坐标
 allYcoordItems = xDoc.getElementsByTagName('cy');  % y坐标
-capacity = xDoc.getElementsByTagName('capacity');  % 车容量
-max_travel_time = xDoc.getElementsByTagName('max_travel_time');  % 车辆的最长运行时间
+capacityNode = xDoc.getElementsByTagName('capacity');  % 车容量
+max_travel_timeNode = xDoc.getElementsByTagName('max_travel_time');  % 车辆的最长运行时间
 allStarttimeItems = xDoc.getElementsByTagName('start');  % 时间窗的起始时间
 allEndtimeItems = xDoc.getElementsByTagName('end');  % 时间窗的终止时间
 allQuantityItems = xDoc.getElementsByTagName('quantity');  % 货物需求量
 allServiceTimeItems = xDoc.getElementsByTagName('service_time');  % 服务时间
 
+% 读取货车容量
+data = char(capacityNode.item(0).getFirstChild.getData);
+capacity = str2num(data);
+
+% 读取车辆的最长运行时间
+data = char(max_travel_timeNode.item(0).getFirstChild.getData);
+max_travel_time = str2num(data);
+
 % 读取x坐标
 for i = 0 : allXcoordItems.getLength - 1
+    data = char(allXcoordItems.item(i).getFirstChild.getData);
     if i == 0  % 仓库坐标
-        depotx = char(allXcoordItems.item(i).getData);
+        depotx = str2num(data);
     else
-        cx = [cx, char(allXcoordItems.item(i).getData)];
+        cx = [cx, str2num(data)];
     end
 end
 
 % 读取y坐标
 for i = 0 : allYcoordItems.getLength - 1
+    data = char(allYcoordItems.item(i).getFirstChild.getData);
     if i == 0  % 仓库坐标
-        depoty = char(allYcoordItems.item(i).getData);
+        depoty = str2num(data);
     else
-        cy = [cy, char(allYcoordItems.item(i).getData)];
+        cy = [cy, str2num(data)];
     end
 end
 
 % 读取时间窗开始时间
 for i = 0 : allStarttimeItems.getLength - 1
-    start_time = [start_time, char(allStarttimeItems.item(i).getData)];
+    data = char(allStarttimeItems.item(i).getFirstChild.getData);
+    start_time = [start_time, str2num(data)];
 end
 
 % 读取时间窗结束时间
 for i = 0 : allEndtimeItems.getLength - 1
-    end_time = [end_time, char(allEndtimeItems.item(i).getData)];
+    data = char(allEndtimeItems.item(i).getFirstChild.getData);
+    end_time = [end_time, str2num(data)];
 end
 
 % 读取顾客点货物需求量
 for i = 0 : allQuantityItems.getLength - 1
-    quantity = [quantity, char(allQuantityItems.item(i).getData)];
+    data = char(allQuantityItems.item(i).getFirstChild.getData);
+    quantity = [quantity, str2num(data)];
 end
 
 % 读取顾客点服务时间
 for i = 0 : allServiceTimeItems.getLength - 1
-    service_time = [service_time, char(allServiceTimeItems.item(i).getData)];
+    data = char(allServiceTimeItems.item(i).getFirstChild.getData);
+    service_time = [service_time, str2num(data)];
 end
 
 % 保存为.mat格式
-save(infilename, 'cx', 'cy', 'depotx', 'depoty', 'start_time', 'end_time', 'quantity', 'service_time', 'capacity', 'max_travel_time');
+S = regexp(infilename, '\.', 'split');  % 去掉.xml后缀
+savefilename = char(S(1));
+save(savefilename, 'cx', 'cy', 'depotx', 'depoty', 'start_time', 'end_time', 'quantity', 'service_time', 'capacity', 'max_travel_time');
 
+% 批量读取数据
+dir C:\Users\cfinsBear\Documents\DDVRPBTW\Solomon-Benchmark\solomon-1987-rc1
+list=dir(['F:\数据\','*.xls']);
+k=length(list)
+ for i=1:k
+     str= strcat ('F:\数据\', list(i).name)
+     a{i}=xlsread(str);
+ end
+ 
+ save(filename, variables);  % filename可以指定路径
 
 
 % allCoordinatesItems = xDoc.getElementsByTagName_r('node'); % 把坐标的数据给读取出来
